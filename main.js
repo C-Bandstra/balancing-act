f73f5b6a9498879b423b16461c720c344b05173f
Charlie Bandstra <bandstracharlie@gmail.com>
2019-11-27 23:50:11 -0600

    Changes from 11/27/19

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var banner = document.querySelector(".welcome-banner");
-var icon1 = document.querySelector(".icon-1");
-var icon2 = document.querySelector(".icon-2");
-var main1 = document.querySelector(".main1")
-var main2 = document.querySelector(".main2")
+var icon1 = document.querySelector(".dashboard-icon");
+var icon2 = document.querySelector(".trans-icon");
+var main1 = document.querySelector(".page-1-main-1")
+var main2 = document.querySelector(".page-1-main-2")
+var main3 = document.querySelector(".page-2-main-1")
+var main4 = document.querySelector(".page-2-main-2")
+
 
 
 document.querySelector(".close-icon").addEventListener("click", closeBanner);
@@ -10,16 +13,20 @@ function closeBanner(){
   banner.style.display= "none";
 }
 
-document.querySelector(".icon-2").addEventListener("click", highlightIcon2);
+document.querySelector(".trans-icon").addEventListener("click", highlightIcon2);
 function highlightIcon2(){
   icon1.style.borderLeft= "none"
   icon2.style.borderLeft= "solid #1CA3BA"
   main1.style.visibility="hidden"
   main2.style.visibility="hidden"
+  
+
+
+
 
 }
 
-document.querySelector(".icon-1").addEventListener("click", highlightIcon1);
+document.querySelector(".dashboard-icon").addEventListener("click", highlightIcon1);
 function highlightIcon1(){
   icon1.style.borderLeft= "solid #1CA3BA"
   icon2.style.borderLeft= "none"
